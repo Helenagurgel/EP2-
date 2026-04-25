f9d97d65c72037baea11804b8a7c86b92be56a5d
felipelimaaa <felipegsl@al.insper.edu.br>
2026-04-25 14:55:31 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,22 @@ def rolar_dados(n):
         r = random.randint(1,6)
         lista.append(r)
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    lista_grande = []
+    dados_rolados_novo = []
+    # for dado_para_guardar in range(len(dados_rolados)):
+    if dado_para_guardar in range(len(dados_rolados)):
+        dados_no_estoque.append(dados_rolados[dado_para_guardar])
+
+    for i in range(len(dados_rolados)):
+        if i != dado_para_guardar:
+            dados_rolados_novo.append(dados_rolados[i])
+
+
+
+
+
+
+    lista_grande =  [dados_rolados_novo, dados_no_estoque]
+    return lista_grande
